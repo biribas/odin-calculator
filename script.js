88e95f85a6fee3c1a8b50bd3f9257bf7d81d7e8d
Carlos Adriano <cadrianogamac@gmail.com>
2022-06-29 16:49:41 -0300

    Change function names

--- script.js
+++ script.js
@@ -17,34 +17,34 @@ function main() {
   const currentValue = document.querySelector('#currentValue');
   const equation = document.querySelector('#equation');
 
-  [...numberButtons].forEach(button => button.addEventListener('click', e => handleNumber(e.target.innerText, currentValue)));
-  [...operatorButtons].forEach(button => button.addEventListener('click', e => handleOperator(e.target.innerText, info, currentValue, equation)));
-  equalButton.addEventListener('click', () => handleEqual(info, currentValue, equation));
+  [...numberButtons].forEach(button => button.addEventListener('click', e => addNumber(e.target.innerText, currentValue)));
+  [...operatorButtons].forEach(button => button.addEventListener('click', e => addOperator(e.target.innerText, info, currentValue, equation)));
+  equalButton.addEventListener('click', () => evaluate(info, currentValue, equation));
   
-  deleteButton.addEventListener('click', () => handleDelete(currentValue));
-  clearButton.addEventListener('click', () => handleClear(info, currentValue, equation));
+  deleteButton.addEventListener('click', () => deleteInput(currentValue));
+  clearButton.addEventListener('click', () => clear(info, currentValue, equation));
   
-  signButton.addEventListener('click', () => handleSign(currentValue));
-  pointButton.addEventListener('click', () => handlePoint(currentValue));
+  signButton.addEventListener('click', () => toggleSign(currentValue));
+  pointButton.addEventListener('click', () => addPoint(currentValue));
 }
 
 document.addEventListener('DOMContentLoaded', main);
 
-function handleNumber(number, currentValue) {
+function addNumber(number, currentValue) {
   if (currentValue.innerText === '0' || !isNum(currentValue.innerText))
     currentValue.innerText = number;
   else if (currentValue.innerText.length < 15)
     currentValue.innerText = currentValue.innerText + number;
 }
 
-function handleOperator(operator, info, currentValue, equation) {
+function addOperator(operator, info, currentValue, equation) {
   info.currentOperator = operator;
   info.previousValue = currentValue.innerText;
   currentValue.innerText = '0';
   equation.innerText = `${info.previousValue} ${operator}`;
 }
 
-function handleEqual(info, currentValue, equation) {
+function evaluate(info, currentValue, equation) {
   if (info.currentOperator === '') return;
   const operation = handleOperation(info.currentOperator);
   const result = operation(+info.previousValue, +currentValue.innerText);
@@ -77,23 +77,23 @@ function handleOperation(operator) {
   }
 }
 
-function handleDelete(currentValue) {
+function deleteInput(currentValue) {
   currentValue.innerText = currentValue.innerText.slice(0, -1);
   if (currentValue.innerText.length === 0) 
     currentValue.innerText = '0';
 }
 
-function handleClear(info, currentValue, equation) {
+function clear(info, currentValue, equation) {
   info.currentOperator = info.previousValue = '';
   currentValue.innerText = '0';
   equation.innerText = '';
 }
 
-function handleSign(currentValue) {
+function toggleSign(currentValue) {
   currentValue.innerText = -1 * (+currentValue.innerText);
 }
 
-function handlePoint(currentValue) {
+function addPoint(currentValue) {
   if (currentValue.innerText.includes('.')) return;
   currentValue.innerText = currentValue.innerText + '.';
 }
