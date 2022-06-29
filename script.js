145fe248dbc15fe947644a42e1929d5d2ff1d641
Carlos Adriano <cadrianogamac@gmail.com>
2022-06-29 15:43:27 -0300

    Create operation logic

--- script.js
+++ script.js
@@ -13,13 +13,14 @@ function main() {
 
   const signButton = document.querySelector('#sign');
   const pointButton = document.querySelector('#point');
-  const equalButotn = document.querySelector('#equal');
+  const equalButton = document.querySelector('#equal');
 
   const currentValue = document.querySelector('#currentValue');
   const equation = document.querySelector('#equation');
 
   [...numberButtons].forEach(button => button.addEventListener('click', e => handleNumber(e.target.innerText, currentValue)));
   [...operatorButtons].forEach(button => button.addEventListener('click', e => handleOperator(e.target.innerText, info, currentValue, equation)));
+  equalButton.addEventListener('click', () => handleEqual(info, currentValue, equation));
 }
 
 function handleNumber(number, currentValue) {
@@ -33,7 +34,32 @@ function handleOperator(operator, info, currentValue, equation) {
   info.currentOperator = operator;
   info.previousValue = currentValue.innerText;
   currentValue.innerText = '0';
-  equation.innerText = `${info.previousValue} ${info.currentOperator}`;
+  equation.innerText = `${info.previousValue} ${operator}`;
+}
+
+function handleEqual(info, currentValue, equation) {
+  const operation = handleOperation(info.currentOperator);
+  const result = operation(+info.previousValue, +currentValue.innerText)
+  equation.innerText = `${info.previousValue} ${info.currentOperator} ${currentValue.innerText} =`;
+  info.previousValue = currentValue.innerText;
+  currentValue.innerText = result;
+}
+
+function handleOperation(operator) {
+  switch (operator) {
+    case '+':
+      return (a, b) => a + b;
+    case '-':
+      return (a, b) => a - b;
+    case 'x':
+      return (a, b) => a * b;
+    case '÷':
+      return (a, b) => a / b;
+    case 'mod': 
+      return (a, b) => a % b;
+    default:
+      return () => console.error('Invalid Operator');
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main);
